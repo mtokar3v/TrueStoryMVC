f7a2bfed40fa529375f7a4e98a54d556690e46a3
mtokar3v <mtokar3v@gmail.com>
2021-09-05 12:17:39 +0300

    add get request to send post model from post constructor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,67 +32,81 @@ namespace TrueStoryMVC.Controllers
             return View();
         }
 
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> Add(PostModel pvm)
+        public async Task<IActionResult> CreatePost([FromBody] PostModel postModel)
         {
-            Post post = new Post { Header = pvm.Header, PostTime = DateTime.Now.ToUniversalTime(), Author = User.Identity.Name };
-            if (!String.IsNullOrEmpty(pvm.Text)) post.Text = pvm.Text;
-            db.Posts.Add(post);
-            await db.SaveChangesAsync();
+            Console.WriteLine(postModel.Header);
+            Console.WriteLine(postModel.TagsLine);
 
-            if (pvm.Image != null)
-            {
-                foreach (IFormFile formFile in pvm.Image)
-                {
-                    //мне эта часть не нравится:
-                    // 1) приходится открывать два потока, с одним не работает, так как b.save() плохо работает и не перезаписывает байты (по моему мнению)
-                    // 2) чтобы считать с формы, я сначала из возвращаемого потока stream(метод openreadstream) создаю массив байт, после чего из него создаю поток memorystream
-                    byte[] bytes = new byte[formFile.Length];
-                    formFile.OpenReadStream().Read(bytes, 0, (int)formFile.Length);
-                    using (var reader = new MemoryStream(bytes))
-                    {
-                        using (Image image = Image.FromStream(reader))
-                        {
+            foreach (var i in postModel.Texts)
+                Console.WriteLine(i);
 
-                            int w = image.Width;
-                            int h = image.Height;
+            foreach (var i in postModel.Scheme)
+                Console.WriteLine(i);
 
-                            double k = (double)h / w;
-                            w = 600;
-                            h = (int)(w * k);
-
-                            using (Bitmap b = new Bitmap(image, w, h))
-                            {
-                                using (var reader2 = new MemoryStream())
-                                {
-                                    b.Save(reader2, System.Drawing.Imaging.ImageFormat.Jpeg);
-                                    bytes = new byte[reader2.ToArray().Length];
-                                    bytes = reader2.ToArray();
-                                }
-                            }
-
-                            ImageInfo img = new ImageInfo()
-                            {
-                                Data = bytes,
-                                Width = w,
-                                Height = h,
-                                PostId = post.Id
-                            };
-
-                            db.Images.Add(img);
-                        }
-                    }
-                }
-            }
-
-            if (!String.IsNullOrEmpty(pvm.TagsLine))
-            {
-                post.Tags = pvm.TagsLine;
-            }
-            await db.SaveChangesAsync();
-            return RedirectToAction("Hot");
+            return Ok();
         }
+
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<IActionResult> Add(PostModel pvm)
+        //{
+        //    Post post = new Post { Header = pvm.Header, PostTime = DateTime.Now.ToUniversalTime(), Author = User.Identity.Name };
+        //    if (!String.IsNullOrEmpty(pvm.Text)) post.Text = pvm.Text;
+        //    db.Posts.Add(post);
+        //    await db.SaveChangesAsync();
+
+        //    if (pvm.Image != null)
+        //    {
+        //        foreach (IFormFile formFile in pvm.Image)
+        //        {
+        //            //мне эта часть не нравится:
+        //            // 1) приходится открывать два потока, с одним не работает, так как b.save() плохо работает и не перезаписывает байты (по моему мнению)
+        //            // 2) чтобы считать с формы, я сначала из возвращаемого потока stream(метод openreadstream) создаю массив байт, после чего из него создаю поток memorystream
+        //            byte[] bytes = new byte[formFile.Length];
+        //            formFile.OpenReadStream().Read(bytes, 0, (int)formFile.Length);
+        //            using (var reader = new MemoryStream(bytes))
+        //            {
+        //                using (Image image = Image.FromStream(reader))
+        //                {
+
+        //                    int w = image.Width;
+        //                    int h = image.Height;
+
+        //                    double k = (double)h / w;
+        //                    w = 600;
+        //                    h = (int)(w * k);
+
+        //                    using (Bitmap b = new Bitmap(image, w, h))
+        //                    {
+        //                        using (var reader2 = new MemoryStream())
+        //                        {
+        //                            b.Save(reader2, System.Drawing.Imaging.ImageFormat.Jpeg);
+        //                            bytes = new byte[reader2.ToArray().Length];
+        //                            bytes = reader2.ToArray();
+        //                        }
+        //                    }
+
+        //                    ImageInfo img = new ImageInfo()
+        //                    {
+        //                        Data = bytes,
+        //                        Width = w,
+        //                        Height = h,
+        //                        PostId = post.Id
+        //                    };
+
+        //                    db.Images.Add(img);
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    if (!String.IsNullOrEmpty(pvm.TagsLine))
+        //    {
+        //        post.Tags = pvm.TagsLine;
+        //    }
+        //    await db.SaveChangesAsync();
+        //    return RedirectToAction("Hot");
+        //}
 
         public async Task<IActionResult> Delete(int? id)
         {
--- Models/ViewModels/PostModel.cs
+++ Models/ViewModels/PostModel.cs
@@ -10,8 +10,10 @@ namespace TrueStoryMVC.Models.ViewModels
     public class PostModel
     {
         public string Header { get; set; }
-        public string Text { get; set; }
+        public List<string> Texts { get; set; }
+        public List<string> Images { get; set; }
         public string TagsLine { get; set; }
-        public List<IFormFile> Image { get; set; }
+        public List<string> Scheme { get; set; }
+        
     }
 }
